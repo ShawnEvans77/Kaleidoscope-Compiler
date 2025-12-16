253a7f68dc10c4082e9d62c0bb36e3d5de8ae3bf
Shawn Evans <shawnevans328@gmail.com>
2025-12-15 22:13:32 -0500

    compiler

---
+++ README.md
@@ -0,0 +1,2 @@
+The difference between an amateur and a professional is that you write your own compiler... so I'll learn how to write
+my own compiler!
---
+++ lexer.cpp
@@ -0,0 +1,4 @@
+// when we want to compile a language, the first thing that must be done is parsing tokens to understand
+// what they are. categorizing keywords appropiately. this is the job of a "LEXER."
+
+// we will be compiling KALEIDOSCOPE, where the only thing of note is a 64-bit floating point type.
